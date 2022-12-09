c62182dbce3834942074ed944f1f24944abd108d
lihuan <izy@huanlis-MacBook-Air.local>
2022-12-10 01:17:31 +0800

    123

--- components.d.ts
+++ components.d.ts
@@ -7,10 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    IconLeft: typeof import('@icon-park/vue-next')['Left']
-    IconPlus: typeof import('@icon-park/vue-next')['Plus']
-    IconRight: typeof import('@icon-park/vue-next')['Right']
-    IconSetting: typeof import('@icon-park/vue-next')['Setting']
     IconSettingConfig: typeof import('@icon-park/vue-next')['SettingConfig']
   }
 }
